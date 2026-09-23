396caa77cffacf5cad37d42114d51247d89e4c74
Conrad008 <kipngeno7180@gmail.com>
2026-09-23 16:35:33 +0300

    refactor: simplify App component structure and remove unused providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './context/AuthContext'
 import ProtectedRoute from './auth/ProtectedRoute'
-import { ThemeProvider } from './context/ThemeContext'
-// import AppShell from './components/layout/AppShell'
-// import Login from './pages/Login'
-// import Dashboard from './pages/Dashboard'
+import AppShell from './components/layout/Appshell'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Shifts from './pages/Shifts'
+import ShiftDetailPage from './pages/ShiftDetailPage'
 
 const queryClient = new QueryClient()
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <AuthProvider>
-            <Router>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route element={<ProtectedRoute />}>
-                  <Route element={<AppShell />}>
-                    <Route index element={<Dashboard />} />
-                  </Route>
+    <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route element={<ProtectedRoute />}>
+                <Route element={<AppShell />}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="/shifts" element={<Shifts />} />
+                  <Route path="/shifts/:id" element={<ShiftDetailPage />} />
                 </Route>
-              </Routes>
-            </Router>
-          </AuthProvider> 
-        </ThemeProvider>
-      </QueryClientProvider>
-    </>
+              </Route>
+            </Routes>
+          </Router>
+    </QueryClientProvider>
   )
 }
-
-export default App
