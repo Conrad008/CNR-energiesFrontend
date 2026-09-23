16891f785e0b39c7181ef177d7c6e2defe60b7b5
Conrad008 <kipngeno7180@gmail.com>
2026-09-23 16:37:24 +0300

    feat: wrap App component with ThemeProvider and AuthProvider for context management

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
