af00e71e2ca2968a972ead8eadc2827f01272cd2
Conrad008 <kipngeno7180@gmail.com>
2026-09-23 14:56:06 +0300

    fix: reorder imports and adjust configuration structure in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: { '/api': 'http://127.0.0.1:8000' },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  server: { proxy: { '/api': 'http://127.0.0.1:8000' } },
 })
