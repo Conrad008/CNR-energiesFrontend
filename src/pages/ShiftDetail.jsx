88fbdeaba3161f32ec8b1ee5b85bf21bbce63e39
Conrad008 <kipngeno7180@gmail.com>
2026-09-25 13:08:11 +0300

    feat: add ShiftDetail component with necessary imports

---
+++ src/pages/ShiftDetail.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import api from '@/api/client'
+import CloseMetersForm from '@/features/shifts/CloseMetersForm'
+import { formatDateTime, formatKsh } from '@/lib/format'
+import Badge from '@/components/ui/Badge'
